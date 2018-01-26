ba1ac342c29f03a8c7e7d7e4219c7fbb161bf73e
Tom Tintel <tintel.tom@gmail.com>
2018-01-26 06:04:56 +0100

    Form validation added to Betaling.js and BillForm.js

--- src/components/BillForm.js
+++ src/components/BillForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, TextInput, ScrollView, TouchableHighlight, View, Button,Picker} from 'react-native';
+import { Text, TextInput, ScrollView, TouchableHighlight, View, Button,Picker, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import { addExpense, addExpenseObject } from '../redux/actions/expenses';
 import { clearexpense} from '../redux/actions/expense';
@@ -15,9 +15,14 @@ import Betaling from '../components/Betaling';
 
  
     addexpenseobject() {
-      this.props.onAddExpenseObject(this.props.expense, this.props.navigation.state.params.trip.id,this.state.description,this.state.categorie);
-      this.props.onClearExpenseObject();
-      this.props.navigation.goBack(null);
+      const isValid = this.validate();
+        if (isValid) {
+          this.props.onAddExpenseObject(this.props.expense, this.props.navigation.state.params.trip.id, this.state.description, this.state.categorie);
+          this.props.onClearExpenseObject();
+          this.props.navigation.goBack(null);
+        } else {
+          Alert(this.state.descriptionError);
+      }
   }
     // items() {
     //   return Object.keys(this.props.items).map(key => this.props.items[key]);
@@ -27,6 +32,23 @@ import Betaling from '../components/Betaling';
    
       return Object.keys(this.props.categories).map(key => this.props.categories[key])
     }
+
+    validate() {
+        let isValid = true; 
+        const errors = {
+            locationError: ''
+        };
+        if (this.state.description.length < 1) {
+            isValid = false;
+            errors.descriptionError = "Description can't be empty";
+        }
+        this.setState({
+            ...this.state,
+            ...this.errors
+        });
+        return isValid;
+    }
+    
   render() {
       console.log(this.props);
      
@@ -36,15 +58,17 @@ import Betaling from '../components/Betaling';
           <View>
           <Text>Beschrijving expense</Text>
                   <TextInput 
-                    onChangeText={(text) => this.setState({description : text})}
+                    onChangeText={(text) => this.setState({description: text })}
                     value={this.state.description}
                   />
                   <Text>Welke Categorie</Text>
                   <Picker
                     selectedValue={this.state.categorie}
-                    onValueChange={(itemValue, itemIndex) => this.setState({categorie: itemValue})}>
+                    onValueChange={(itemValue, itemIndex) => this.setState({categorie: itemValue })}
+                  >
                   {/* <Picker.Item label="Alle" value="alle" /> */}
-                    { this.categories().map((categorie) => {return <Picker.Item key={categorie.naam} label={categorie.naam} value={categorie.naam}/> })}
+                    { this.categories().map((categorie) => { return <Picker.Item key={categorie.naam} label={categorie.naam} value={categorie.naam} />;
+                  })}
                   </Picker>
 
             <Text>Totale bedrag:</Text>
